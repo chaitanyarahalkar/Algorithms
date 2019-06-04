3a01b767033364efba3cb5c872e3806085e67fc8
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-06-04 20:14:56 +0530

    Fix a minor issue in algorithms stl code

--- Miscellaneous/algorithms.cpp
+++ Miscellaneous/algorithms.cpp
@@ -28,15 +28,15 @@ void algorithms_implementation(){
     for(auto it = v.begin(); it != v.end(); it++)
         cout << *it << "\n";
 
-    cout << "Maximum element " << *max_element(v.begin(),v.end());
-    cout << "Minimum element " << *min_element(v.begin(),v.end());
-    //cout << "Accumulation (Sum) " << accumulate(v.begin(),v.end(),0);
+    cout << "Maximum element " << *max_element(v.begin(),v.end()) << "\n";
+    cout << "Minimum element " << *min_element(v.begin(),v.end()) << "\n";
+    
 
-    cout << "Occurences of " << n << count(v.begin(),v.end(),n);
-    //cout << "Find element " << n - 2 << find(v.begin(),v.end(),n-2) != v.end() ? cout << "Found at " << find(v.begin(),v.end(),n-2) << "\n" : cout << "Not found!\n";
+    cout << "Occurences of " << n << count(v.begin(),v.end(),n) << "\n";
+    cout << "Find element " << n - 2 << (find(v.begin(),v.end(),n-2) != v.end() ? " Found\n" : " Not found!\n");
 
     sort(v.begin(),v.end());
-    cout << "Element found with Binary Search" << binary_search(v.begin(),v.end(), n - 3);
+    cout << "Element found with Binary Search " << binary_search(v.begin(),v.end(), n - 3) << "\n";
 
 }
 int main(void){
